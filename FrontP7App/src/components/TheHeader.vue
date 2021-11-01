ce0d9b439c0d062c2e74d7d5628ca31fd14c634b
FabioDevCode <0677384849@orange.fr>
2021-11-01 16:55:10 +0100

    Ajout du Front-End en Vue.js début du projet

---
+++ FrontP7App/src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header-container">
+        <header class="header-bloc">
+            <img class="img" src="../assets/icon-noir.png" alt="Logo Groupomania">
+            <nav class="">
+                <router-link>Accueil</router-link>
+                <router-link>Publier</router-link>
+                <router-link>Profil</router-link>
+            </nav>
+        </header>
+    </div>
+</template>
+
+
+//------------------------------------
+<script>
+export default {
+    name: 'theheader'
+}
+
+
+</script>
+
+
+//------------------------------------
+<style scoped>
+
+
+
+</style>
